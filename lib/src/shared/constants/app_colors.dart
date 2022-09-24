f01f982d330ca4e7bf3e50752d5294f57df006a7
Kaio C. de Oliveira <hello@kaio.dev>
2022-09-23 22:13:30 -0300

    adiciona app colors

---
+++ lib/src/shared/constants/app_colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColors {
+  AppColors._();
+
+  static const Color primary = Color(0xFFE2F4FF);
+  static const Color grey = Color(0xFF79838B);
+  static const Color label = Color(0xFFB8C5D0);
+}
